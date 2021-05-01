6853bef4392e02d7b11594de25739c2acfb7f803
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-05-01 15:14:34 +0530

    format code style

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from .models import Item, Cart, CartItem
-# Register your models here.
+
+from .models import Cart, CartItem, Item
+
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
